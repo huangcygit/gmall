ee9b0af3700b2dc4b0172bada881c297d37c2f6f
huangcygit <1013649574@qq.com>
2020-11-30 08:57:49 +0800

    cart

---
+++ gmall-cart-web/src/test/java/com/hy/gmall/gmallcartweb/GmallCartWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.gmall.gmallcartweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCartWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
